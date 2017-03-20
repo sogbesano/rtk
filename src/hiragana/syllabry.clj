f8efbf2e0cf718d81f5ee5a3d67f10d781c335a2
olu sogbesan <sogbesano@gmail.com>
2017-03-20 14:27:02 +0000

    more syllabograms addded to syllabry.clj of /hiragana

--- src/hiragana/syllabry.clj
+++ src/hiragana/syllabry.clj
@@ -9,6 +9,8 @@
                  :consonant-vowel-unions {
                                            :ka "\u304B" :ki "\u304D" :ku "\u304F" :ke "\u3051" :ko "\u3053"
                                            :sa "\u3055" :si "\u3057" :su "\u3059" :se "\u305B" :so "\u305D"
+                                           :ta "\u305F" :ti "\u3061" :tu "\u3063" :te "\u3066" :to "\u3068"
+
                                          }
                  :consonant {}
                 }
