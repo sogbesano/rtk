6d12a953bfab2314249a2df4b30e22432f40bf19
olu sogbesan <sogbesano@gmail.com>
2017-03-20 14:18:28 +0000

    more syllabograms addded to syllabry.clj of /hiragana

--- src/hiragana/syllabry.clj
+++ src/hiragana/syllabry.clj
@@ -1,13 +1,14 @@
 (ns hiragana.syllabry
   (:gen-class))
 
-;;Japanese hiragana syllabry in gojūon ordering.
+;;Japanese hiragana syllabry in unicode with gojūon ordering.
 (defonce syllabograms
   {
     :monographs {
                  :vowels {:a "\u3042" :i "\u3044" :u "\u3046" :e "\u3048" :o "\u304A"}
                  :consonant-vowel-unions {
                                            :ka "\u304B" :ki "\u304D" :ku "\u304F" :ke "\u3051" :ko "\u3053"
+                                           :sa "\u3055" :si "\u3057" :su "\u3059" :se "\u305B" :so "\u305D"
                                          }
                  :consonant {}
                 }
